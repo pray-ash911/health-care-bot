a29b11aa1589769ee84790528e92b44443384cd1
pray-ash911 <rawal48911@gmail.in>
2024-10-18 20:59:08 +0545

    Add modified add_medication.html template

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
+from flask import Flask,render_template,request,redirect
 
-def main():
+from medication_reminder import store_medication,get_all_medications
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    medications = get_all_medications() # get_all_medications chai euta finction ho jun medication_reminder.py ko ho
+    return render_template('index.html',medications = medications) # yo left side ko medications chai index.html ma use hunxa ani index.html chai frontend ho jun render template le grda front ra backedn join grxa
+
+@app.route('/add',methods=['GET','POST'])
+def add_medication():
+    if request.method == 'POST':
+        medication_info = {
+            "name": request.form['name'], # add_medication ma vako user haleko value yeta layeko
+            "dose": request.form['dose'],
+            "frequency": request.form['frequency'],
+            "time": request.form['time']
+        }
+        store_medication(medication_info)#store_medication is imporeted mathi and now stores the new medi
+        return redirect('/')# retun to main page i.e index.html
+    return render_template('add_medication.html')#agadi user lai ta add_medication.html dekhauna lai)
